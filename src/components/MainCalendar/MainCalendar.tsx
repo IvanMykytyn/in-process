e4ec31fe65916e581a5b9e528cd3d20fce2fdc17
Omelaa <Ostpa28@gmail.com>
2022-10-11 16:15:11 +0300

    made-a-choice-between-the-calendars

--- src/components/MainCalendar/MainCalendar.tsx
+++ src/components/MainCalendar/MainCalendar.tsx
@@ -8,8 +8,6 @@ import css from './MainCalendar.module.scss';
 const MainCalendar = () => {
     const [value, onChange] = useState(new Date());
 
-    console.log(value)
-
     return (
         <div className={css.calendar}>
             <Calendar onChange={onChange} value={value}/>
