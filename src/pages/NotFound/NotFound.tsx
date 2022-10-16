517e1cc810310c9d59fe5b1a55279cc79f85cbfd
Omelaa <Ostpa28@gmail.com>
2022-10-16 20:39:49 +0300

    'notFoundPage'

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {FC} from 'react'
+import {NavLink, useNavigate} from 'react-router-dom';
 
 // styles
-import cn from 'classnames'
-import css from './not-found.module.scss'
+import css from './NotFound.module.scss'
 
-const NotFound = () => {
-  return <div>NotFound</div>
+import {Button} from "./../../components/index";
+
+const NotFound: FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className={css.error}>
+            <div className={css.error__wrapper}>
+                <h3 className={css.error__title}>
+                    404
+                    <span className={css.error__description}>
+                        Page Not Found :(
+                    </span>
+                </h3>
+                <NavLink to={`${navigate(-1)}`}>
+                    <Button>Go Back</Button>
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
-export { NotFound }
+export {NotFound}
