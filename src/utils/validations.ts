9bafb6b24fb61bb09a1ab8ad75f9b5b6c28e2905
Khrystyna Kindrat <102920051+ChristabelKindrat@users.noreply.github.com>
2022-10-21 18:26:44 +0300

    Fix password validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,5 +1,9 @@
 import Joi from 'joi';
 
+const joi = require('joi');
+const { joiPasswordExtendCore } = require('joi-password');
+const joiPassword = joi.extend(joiPasswordExtendCore);
+
 const validateEmail = Joi.string()
     .required()
     .regex(/@incorainc.com\s*$/)
@@ -8,15 +12,17 @@ const validateEmail = Joi.string()
         'string.pattern.base': 'The email can only end with incorainc.com',
     });
 
-const validatePassword = Joi.string()
-    .required()
-    .min(8)
-    .max(255)
-    .regex(/^(?=.?[A-Z])(?=.?[a-z]).{8,255}$/)
-    .messages({
-        'string.empty': 'The password cannot be empty.',
-        'string.pattern.base': 'The password must be longer then eight characters.',
-    });
+const validatePassword = joiPassword
+        .string()
+        .minOfLowercase(2)
+        .minOfUppercase(2)
+        .noWhiteSpaces()
+        .required()
+        .messages({
+            'password.minOfUppercase': 'Password should contain at least {#min} uppercase character',
+            'password.minOfLowercase': 'Password should contain at least {#min} lowercase character',
+            'password.noWhiteSpaces': 'Password should not contain white spaces',
+        })
 
 const validateConfirmPassword = Joi.any().valid(Joi.ref('password')).messages({
     'any.required': 'The password must be the same.',
