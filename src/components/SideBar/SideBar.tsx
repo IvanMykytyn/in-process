1f3ff289a138bcfe1a27e6a7f68b5726116423e4
Khrystyna Kindrat <102920051+ChristabelKindrat@users.noreply.github.com>
2022-10-23 01:14:40 +0300

    modified sidebar component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,97 +1,152 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
+import moment from 'moment';
+import {Accordion} from '@mui/material';
 
 // styles
 import cn from 'classnames';
 import scss from './sidebar.module.scss';
-import {setting, search, user} from '../../assets/images/icons';
 
+import {setting, search, user, clock, calendar} from '../../assets/images/icons';
 import {Input} from '../index'
+
 import {Link} from 'react-router-dom';
 
+interface Data {
+    room: string;
+    answer: {
+        date: string;
+        time: string;
+        room: string;
+        staff: string;
+    };
+}
+
+const data: Data[] = [
+    {
+        room: 'room 1',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '1',
+            staff: 'board, TV, markers'
+        }
+    },
+    {
+        room: 'room 2',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '2',
+            staff: 'board, TV, markers'
+        }
+    },
+    {
+        room: 'room 3',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '3',
+            staff: 'board, TV, markers'
+        }
+    }, {
+        room: 'room 3',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '3',
+            staff: 'board, TV, markers'
+        }
+    }, {
+        room: 'room 4',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '3',
+            staff: 'board, TV, markers'
+        }
+    }, {
+        room: 'room 5',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '3',
+            staff: 'board, TV, markers'
+        }
+    },
+    {
+        room: 'room 6',
+        answer: {
+            date: '2022-10-25',
+            time: '18:30:00',
+            room: '4',
+            staff: 'board, TV, markers'
+        }
+    }
+];
 
 const SideBar = () => {
+
+    const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+    const [selected, setSelected] = useState<number | null>(null);
+
+    const isOpened = () => {
+        setIsOpenMenu(!isOpenMenu);
+    };
+    //TO DO MOMENT.js
+
     return (
-        <div className={cn(scss.inner)}>
-            <ul className={cn(scss.bookmarks)}>
-                <li className={cn(scss.bookmarks__item)}>
-                    <button className={cn(scss.bookmarks__btn)}
-                            type={'button'}>
-                        <Link to={'/dashboard/calendar'}>
-                            Calendar
-                        </Link>
-                    </button>
-                </li>
-                <li className={cn(scss.bookmarks__item)}>
-                    <button className={cn(scss.bookmarks__btn)}
-                            type={'button'}>
-                        <Link to={'/dashboard/timeline'}>
-                            Timeline
-                        </Link>
-                    </button>
-                </li>
-                <li className={cn(scss.bookmarks__item)}>
-                    <button className={cn(scss.bookmarks__btn)}
-                            type={'button'}>
-                        <Link to={'/dashboard/map'}>
-                            Map
-                        </Link>
-                    </button>
-                </li>
-                <li className={cn(scss.bookmarks__item)}>
-                    <button className={cn(scss.bookmarks__btn)}
-                            type={'button'}>
-                        <Link to={'/dashboard/settings'}>
-                            Settings
-                        </Link>
-                    </button>
-                </li>
-            </ul>
-            <div className={cn(scss.sidebar)}>
-                <div className={cn(scss.wrapper)}>
-                    <ul className={cn(scss.info)}>
-                        <li>
-                            <button className={cn(scss.info__settings)}>
+        <div className={isOpenMenu ? `${scss.sidebar}` : `${scss.sidebar} ${scss.hide}`}>
+            <div className={cn(scss.wrapper)}>
+                <button className={cn(scss.burger)} onClick={isOpened}>
+                    Burger
+                </button>
+                <ul className={isOpenMenu ? `${scss.info}` : `${scss.info} ${scss.hide}`}>
+                    <li>
+                        <button className={cn(scss.info__settings)}>
+                            <Link to={'/dashboard/settings'}>
                                 <img src={setting} alt="setting" height={30} width={30}/>
-                            </button>
-                        </li>
-                        <li>
-                            <button className={cn(scss.info__userName)}>
-                                User Name
-                            </button>
-                        </li>
-                        <li>
-                            <button className={cn(scss.info__userImg)}>
-                                <img src={user} alt="user" height={30} width={30}/>
-                            </button>
-                        </li>
-                    </ul>
-                    <div className={cn(scss.input)}>
-                        <Input fullWidth={true} label={'Search...'}/>
-                    </div>
-                    <ul className={cn(scss.booked)}>
-                        <li className={cn(scss.booked__item)}>
-                            room 1
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 2
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 3
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 4
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 5
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 6
-                        </li>
-                        <li className={cn(scss.booked__item)}>
-                            room 7
-                        </li>
-                    </ul>
+                            </Link>
+                        </button>
+                    </li>
+                    <li>
+                        <button className={cn(scss.info__userName)}>
+                            User Name
+                        </button>
+                    </li>
+                    <li>
+                        <button className={cn(scss.info__userImg)}>
+                            <img src={user} alt="user" height={30} width={30}/>
+                        </button>
+                    </li>
+                </ul>
+                <div className={isOpenMenu ? `${scss.input}` : `${scss.input} ${scss.hide}`}>
+                    <Input fullWidth={true} label={'Search...'}/>
                 </div>
+                <ul className={isOpenMenu ? `${scss.booked}` : `${scss.booked} ${scss.hide}`}>
+                    {
+                        data && data.map((room, index) =>
+                            <li className={cn(scss.booked__item)} key={index}>
+                                <li className={cn(scss.booked__info)}>
+                                    <div>
+                                        Room {room.answer.room}
+                                    </div>
+                                    <div className={cn(scss.booked__time)}>
+                                        {moment('2022-10-25 18:30:00').fromNow()}
+                                    </div>
+                                </li>
+                                <div className={cn(scss.booked__time)}>
+                                    <li className={cn(scss.booked__info)}>
+                                        <img src={calendar} alt="Data" height={15} width={15}/>: {room.answer.date}
+                                    </li>
+                                    <li className={cn(scss.booked__info)}>
+                                        <img src={clock} alt="Time" height={15} width={15}/>: {room.answer.time}
+                                    </li>
+                                </div>
+                            </li>
+                        )
+                    }
+
+                </ul>
             </div>
         </div>
     );
