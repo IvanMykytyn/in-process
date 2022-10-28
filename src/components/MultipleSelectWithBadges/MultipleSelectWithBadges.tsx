05f20557959402c1ab939949d5d6a2bb4cc72ffe
Omelaa <81105207+Omelaa@users.noreply.github.com>
2022-10-28 16:23:32 +0300

    Update MultipleSelectWithBadges.tsx

--- src/components/MultipleSelectWithBadges/MultipleSelectWithBadges.tsx
+++ src/components/MultipleSelectWithBadges/MultipleSelectWithBadges.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, FC} from 'react';
+import React, { Dispatch, SetStateAction, FC } from 'react';
 import {
     Autocomplete,
     AutocompleteProps,
@@ -11,50 +11,45 @@ import {
 import './multiple-select-with-badges.styles.scss';
 
 interface CustomProps {
-    options: Array<string>;
-    setSelectedOptions: Dispatch<SetStateAction<Array<string>>>;
-    noOptionsText?: string;
+  options: Array<string>;
+  setSelectedOptions: Dispatch<SetStateAction<Array<string>>>;
+  noOptionsText?: string;
 }
 
-// string[], true, undefined, undefined, React.ElementType<any>
-const MultipleSelectWithBadges: FC<CustomProps &
-    AutocompleteProps<string, true, undefined, undefined, React.ElementType<any>>> = ({
-                                                                                          setSelectedOptions,
-                                                                                          noOptionsText,
-                                                                                          options,
-                                                                                          ...rest
-                                                                                      }) => {
+const MultipleSelectWithBadges: FC<
+  CustomProps &
+    AutocompleteProps<string, true, undefined, undefined, React.ElementType<any>>
+> = ({ setSelectedOptions, noOptionsText, options, ...rest }) => {
+  const handleChange = (e: React.SyntheticEvent, values: string[]) => {
+    setSelectedOptions(values);
+  };
 
-    const handleChange = (e: React.SyntheticEvent, values: string[]) => {
-        setSelectedOptions(values);
-    };
-
-    return (
-        <div className={'multi-select'}>
-            <Autocomplete
-                multiple
-                options={options}
-                noOptionsText={noOptionsText}
-                onChange={handleChange}
-                renderTags={(value: readonly string[], getTagProps) => (
-                    <div className="badges-container">
-                        {value.map((option: string, index: number) => (
-                            <Chip
-                                variant="outlined"
-                                size="small"
-                                label={option}
-                                {...getTagProps({index})}
-                            />
-                        ))}
-                    </div>
-                )}
-                {...rest}
-                renderInput={(params: AutocompleteRenderInputParams) => (
-                    <TextField {...params} />
-                )}
-            />
-        </div>
-    );
+  return (
+    <div className={'multi-select'}>
+      <Autocomplete
+        multiple
+        options={options}
+        noOptionsText={noOptionsText}
+        onChange={handleChange}
+        renderTags={(value: readonly string[], getTagProps) => (
+          <div className="badges-container">
+            {value.map((option: string, index: number) => (
+              <Chip
+                variant="outlined"
+                size="small"
+                label={option}
+                {...getTagProps({ index })}
+              />
+            ))}
+          </div>
+        )}
+        {...rest}
+        renderInput={(params: AutocompleteRenderInputParams) => (
+          <TextField {...params} />
+        )}
+      />
+    </div>
+  );
 };
 
-export {MultipleSelectWithBadges};
+export { MultipleSelectWithBadges };
