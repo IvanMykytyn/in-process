7df076ce7d8441430e5b44070a65d215d543c387
VanekCheck <mukutunivan04@gmail.com>
2022-11-11 15:05:42 +0200

    fixed bug

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,8 +1,19 @@
-import { FC, PropsWithChildren } from 'react';
-import { Navigate } from 'react-router-dom';
-import { isLoggedIn } from 'services';
+import { FC, PropsWithChildren, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { isLoggedIn, NotifyService } from 'services';
+import { selectUser, useAppSelector } from 'store';
 
 const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
+  const { user } = useAppSelector(selectUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      navigate('/login');
+      NotifyService.error('Please login again');
+    }
+  }, [navigate, user]);
+
   if (!isLoggedIn()) {
     return <Navigate to="/login" />;
   }
